79b954a24049e9b4087422ed1924d53f134dc1b8
itsal-me <itsal50505@gmail.com>
2025-05-08 01:01:01 +0600

    initial setup 2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    os.getenv('CORS_ALLOWED_ORIGINS', '').split(',')
 ]
 
 ROOT_URLCONF = 'backend.urls'
