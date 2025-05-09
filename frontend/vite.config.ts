46144547d5be470769d6f1e13982ab77b4d1bf0f
itsal-me <itsal50505@gmail.com>
2025-05-09 16:53:34 +0600

    mvp launch

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,27 +2,19 @@ import { defineConfig } from 'vite'
 import path from "path"
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import autoprefixer from 'autoprefixer'
+
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer(),
-        
-      ],
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: '127.0.0.1', // Use this for IPv4 loopback address
-    port: 5173,         // You can specify the port if necessary
-  }
+  // server: {
+  //   host: '127.0.0.1', // Use this for IPv4 loopback address
+  //   port: 5173,         // You can specify the port if necessary
+  // }
 })
